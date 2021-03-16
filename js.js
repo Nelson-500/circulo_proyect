d0a491bbe9603de747b21cae2c4d1a43c3eb70ea
Jose <josemario833@gmail.com>
2021-03-16 15:37:09 -0600

    Funciones de área y perímetro de un circulo

--- js.js
+++ js.js
@@ -1,7 +1,19 @@
-const app =new Vue({
-el: '#app',
-data: {
-    titulo: 'holamundo Vue'
-}
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: "Calculando el Área y Perímetro de un círculo con Vue.js",
+        radio: "",
+    },
+    computed: {
+        CalcularAreaCirculo: function () {
+            let AreaCirculo = Math.PI * Math.pow(this.radio, 2);
 
+            return this.area = AreaCirculo.toFixed(2);
+        },
+        CalcularPerimetro: function () {
+            let Perimetro = Math.PI * this.radio * 2;
+
+            return this.perimetro = Perimetro.toFixed(2);
+        },
+    }
 })
